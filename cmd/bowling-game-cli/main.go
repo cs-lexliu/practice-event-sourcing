7beddc0d5ae4bedd7add93d42c02dd85ba8d5880
cs-lexliu <lex.liu@thecarousell.com>
2022-08-20 00:04:15 +0800

    add main for bowling-game-cli

---
+++ cmd/bowling-game-cli/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/cs-lexliu/practice-event-sourcing/internal/ddd/adpater"
+	"github.com/cs-lexliu/practice-event-sourcing/pkg/domain/bowling_game/entity"
+	"github.com/cs-lexliu/practice-event-sourcing/pkg/domain/bowling_game/usecase"
+	"github.com/google/uuid"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	eventBus := adpater.NewDomainEventBus()
+	repository := adpater.NewEventRepositoryAdapter[*entity.BowlingGame](eventBus)
+
+	var currentID string
+
+loop:
+	for {
+		fmt.Println("What's your command: create, roll, exit")
+		cmd := getInput(reader)
+		switch cmd {
+		case "create":
+			currentID = uuid.New().String()
+			u := usecase.NewCreateBowlingGameUseCase(repository)
+			if err := u.Execute(context.Background(), currentID); err != nil {
+				fmt.Println(err)
+				break loop
+			}
+		case "roll":
+			fmt.Println("How many pins you hit? 0 - 10")
+			hitStr := getInput(reader)
+			hit, err := strconv.Atoi(hitStr)
+			if err != nil {
+				fmt.Println(err)
+				break loop
+			}
+			u := usecase.NewRollOneBallUseCase(repository)
+			if err := u.Execute(context.Background(), currentID, hit); err != nil {
+				fmt.Println(err)
+				break loop
+			}
+		case "exit":
+			break loop
+		}
+		b, err := repository.FindByID(context.Background(), currentID)
+		if err != nil {
+			fmt.Println(err)
+			break loop
+		}
+		fmt.Printf("Your scores is %d!!!\n", b.Scores())
+	}
+}
+
+func getInput(reader *bufio.Reader) string {
+	cmd, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSuffix(cmd, "\n")
+}
